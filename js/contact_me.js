45cc7963d005c6132c2981620b5cdb78f30a9600
Guilherme Araújo <guilhermeama@gmail.com>
2020-07-19 19:38:02 -0300

    Update domain

--- js/contact_me.js
+++ js/contact_me.js
@@ -19,7 +19,7 @@ $(function() {
         firstName = name.split(' ').slice(0, -1).join(' ');
       }
       $.ajax({
-        url: "//formspree.io/contact@guilhermearaujo.me",
+        url: "//formspree.io/contact@guilhermearaujo.dev",
         method: "POST",
         data: {
           name: name,
